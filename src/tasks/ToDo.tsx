681d3ac9093fb4a532e39c2ac47ab5989981bc96
barmaglot71 <barmaglot71@gmail.com>
2019-09-11 17:52:56 +0300

    add delete button functionality

--- src/tasks/ToDo.tsx
+++ src/tasks/ToDo.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-
 import '../App.css';
-import Task from './Task';
 import Button from './Button';
-import list from "./TaskList";
-import Input from "./Input";
+
 
 type ToDoItem = {
     text: string;
@@ -24,31 +21,44 @@ class ToDo extends React.Component<{}, ToDoState> {
         this.setState({
             list: [
                 ...this.state.list,
-                { text:this.state.text, active: true, id: this.state.list.length + 1 },
+                { text:this.state.text, active: true, id: Date.now() },
             ],
             text:"",
         });
 
     };
      onInputChange = (e:React.FormEvent<HTMLInputElement>) => {
-         console.log(e.currentTarget.value);
          this.setState({text: e.currentTarget.value})
      };
 
+     onDeleteBtnHandler = (id: number)=> () => {
+          this.setState( {list: this.state.list.filter(task => task.id !== id)
+              } )
+         };
+
+
+     onClickhandler = (id: number)=> () => { this.setState({list: this.state.list[this.state.list.findIndex(task => task.id == id)].active: false)
+                  };
+
+
     render() {
-        let todoList =this.state.list.map((task)=> <li key={task.id}>{task.text}</li>);
         const line = <hr/>;
+        let todoList =this.state.list.map((task)=> <li key={task.id}>{task.text}
+        <Button text = "Delete" handler={
+            this.onDeleteBtnHandler(task.id)}/>
+            {line}
+        </li>);
 
         return (
                 <div className="ToDo">
-
                     <h1>Todo List</h1>
                     <input type="text" placeholder="Task" onChange={this.onInputChange} value={this.state.text}/>
                     <Button text="Add" handler={
                         this.addNew}/>
+
                     {todoList}
                     {/*<Input addNew={this.addNew} task={this.state.text}/>*/}
-                    {line}
+
                 </div>
         );
     }
